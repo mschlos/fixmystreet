c17410529c93d2871c1bc11411f078cd39dbd7c1
Steven Day <steve@mysociety.org>
2012-11-09 09:55:12 +0000

    Remove command that always fails and replace with instructions to run after login

--- bin/provision.sh
+++ bin/provision.sh
@@ -12,12 +12,10 @@ sed "s/^MAPIT_URL: ''/MAPIT_URL: 'http:\/\/mapit.mysociety.org'/" -i /var/www/lo
 sed "s/^  - cobrand_one/  - fixmystreet/" -i /var/www/localhost/fixmystreet/conf/general.yml
 sed "s/^  - cobrand_two: 'hostname_substring2'/  - fixmystreet: 'localhost'/" -i /var/www/localhost/fixmystreet/conf/general.yml
 
-# Run the update-all-reports script
-/var/www/localhost/fixmystreet/bin/cron-wrapper /var/www/localhost/fixmystreet/bin/update-all-reports
-
 # Generate Welsh and Norweigian locales so i8n tests pass
 locale-gen cy_GB.UTF-8
 locale-gen nb_NO.UTF-8
 
 echo "You can now ssh into your vagrant box: vagrant ssh"
 echo "The website code is found in: /var/www/localhost/fixmystreet or ~/fixmystreet"
+echo "Note: to get a fully up and running system, you may want to run bin/cron-wrapper bin/update-all-reports after you login"
